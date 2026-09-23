a87ba6a0669642c69e94dfdb37226eeffa5110c9
Trishul192004 <pes1ug23cs658@pesu.pes.edu>
2026-09-23 22:48:13 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0 ;
+        int ten = 0;
+
+        for(int bill : bills){
+
+            if(bill == 5){
+                five++;
+            }
+            else if(bill == 10){
+                if(five == 0) return false;
+                five--;
+                ten++;
+            }
+            else if(bill == 20){
+                if(five > 0 && ten > 0){
+                    ten--;
+                    five--;
+                }
+
+                //otherwise use 3 * 5$
+                else if(five >= 3){
+                    five -= 3;
+                }
+
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
