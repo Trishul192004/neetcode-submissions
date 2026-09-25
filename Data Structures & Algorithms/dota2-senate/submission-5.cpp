bcd57631a327a848adf5a40d5041d32ab4576dd7
Trishul192004 <pes1ug23cs658@pesu.pes.edu>
2026-09-25 12:13:39 +0530

    Add: dota2-senate - submission-5

---
+++ Data Structures & Algorithms/dota2-senate/submission-5.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n = senate.size();
+
+        queue<int>radiant;
+        queue<int>dire;
+
+        //store indices
+        for(int i =0 ;i < n ;i++){
+            if(senate[i] == 'R') radiant.push(i);
+            if(senate[i] == 'D') dire.push(i);
+        }
+
+        while(!radiant.empty() && !dire.empty()){
+            int r = radiant.front();
+            int d = dire.front();
+
+            radiant.pop();
+            dire.pop();
+
+            if(r< d){
+                radiant.push(r + n);
+            }
+            else{
+                dire.push(d + n);
+            }
+        }
+        
+
+        return (radiant.empty()) ? "Dire" : "Radiant";
+    }
+};
